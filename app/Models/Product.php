9b7a2ec83bcee2b1f18dfcdd8dbf68248dfac7a1
Dipak Mahatara <diplove32928@gmail.com>
2023-05-04 17:50:38 +0545

    Product model added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'description', 'price', 'subcategory_id'];
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
 }
